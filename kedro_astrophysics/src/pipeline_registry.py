8655393444189f8a0587bc5a82ab6c5ebed7f6a6
Alexandra Wernersson <47984687+Alexandra-Wernersson@users.noreply.github.com>
2025-08-31 17:16:22 +0200

    Update pipeline_registry.py

--- kedro_astrophysics/src/pipeline_registry.py
+++ kedro_astrophysics/src/pipeline_registry.py
@@ -1,15 +1,17 @@
 """Project pipelines."""
 from __future__ import annotations
 
-from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+from pipelines import astrophysics_data, astrophysics_sampler
 
-from kedro_workshop.pipelines import astrophysics_data, astrophysics_sampler
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines."""
+    data_pipeline = astrophysics_data.create_pipeline()
+    sampler_pipeline = astrophysics_sampler.create_pipeline()
+
     return {
-        "__default__": astrophysics_data.create_pipeline(),
-        "astrophysics_data": astrophysics_data.create_pipeline(),
-        "astrophysics_sampler": astrophysics_sampler.create_pipeline(),
+        "__default__": data_pipeline + sampler_pipeline,
+        "astrophysics_data": data_pipeline,
+        "astrophysics_sampler": sampler_pipeline,
     }
